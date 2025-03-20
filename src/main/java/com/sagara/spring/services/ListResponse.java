6abf65350f8e6c34db9e2bcbc7abcde9d7db4b3b
drnhsproject <dian.rahmat74@gmail.com>
2025-03-20 11:08:13 +0700

    fix(service): list response

--- src/main/java/com/sagara/spring/services/ListResponse.java
+++ src/main/java/com/sagara/spring/services/ListResponse.java
@@ -1,33 +1,7 @@
 package com.sagara.spring.services;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.List;
 
-@Setter
-@Getter
-public class ListResponse<D> {
-
-    private DataWrapper<D> data;
-
-    public ListResponse(String message, List<D> result, Long count) {
-        this.data = new DataWrapper<>(message, result, count);
-    }
-
-    @Setter
-    @Getter
-    public static class DataWrapper<D> {
-
-        private String message;
-        private List<D> result;
-        private Long count;
-
-        public DataWrapper(String message, List<D> result, Long count) {
-            this.message = message;
-            this.result = result;
-            this.count = count;
-        }
-
-    }
-}
+public record ListResponse<D>(
+        String message, List<D> data, Long count
+) {}
