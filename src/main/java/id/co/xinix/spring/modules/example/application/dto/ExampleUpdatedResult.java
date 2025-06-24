b1eeb5b277f86349781ba90194eacdca433c5c4f
Naufal Nur Hidayat <naufalnurhidayat@Naufals-MacBook-Pro.local>
2025-06-24 16:33:03 +0700

    refactor(example): fixing edit and soft delete

--- src/main/java/id/co/xinix/spring/modules/example/application/dto/ExampleUpdatedResult.java
+++ src/main/java/id/co/xinix/spring/modules/example/application/dto/ExampleUpdatedResult.java
@@ -1,4 +1,27 @@
 package id.co.xinix.spring.modules.example.application.dto;
 
-public record ExampleUpdatedResult(Long id, String code, String name, Integer age) {
-}
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public record ExampleUpdatedResult(
+    Long id,
+    String code,
+    String name,
+    String nik,
+    String hobbies,
+    Integer citizen,
+    String phone,
+    Integer age,
+    String taxpayerNumber,
+    LocalDate date,
+    Boolean marriedStatus,
+    String gender,
+    String checkbox,
+    String inputDateYear,
+    LocalTime inputDate,
+    String address,
+    String ProfilePicture,
+    String MultipleImage,
+    String SupportingDocument,
+    String MultipleFile
+) {}
--- src/main/java/id/co/xinix/spring/modules/example/application/usecase/ArchiveExample.java
+++ src/main/java/id/co/xinix/spring/modules/example/application/usecase/ArchiveExample.java
@@ -3,26 +3,24 @@ package id.co.xinix.spring.modules.example.application.usecase;
 import id.co.xinix.spring.UseCase;
 import id.co.xinix.spring.exception.NotFoundException;
 import id.co.xinix.spring.modules.example.application.dto.ExampleDTO;
+import id.co.xinix.spring.modules.example.domain.Example;
+import id.co.xinix.spring.modules.example.domain.ExampleRepository;
 import id.co.xinix.spring.modules.example.domain.service.ExampleService;
+import lombok.AllArgsConstructor;
 
 @UseCase
+@AllArgsConstructor
 public class ArchiveExample {
 
-    private final ExampleService exampleService;
-
-    public ArchiveExample(ExampleService exampleService) {
-        this.exampleService = exampleService;
-    }
+    private final ExampleRepository exampleRepository;
 
     public void handle(Long id) {
-        ExampleDTO example = exampleService.findOne(id)
-                .map(exists -> {
-                    exists.setIsActive(true);
-                    exists.setStatus(0);
+        Example example = exampleRepository.findById(id)
+            .orElseThrow(() -> new NotFoundException("example not found"));
 
-                    return exists;
-                }).orElseThrow(() -> new NotFoundException("example not found"));
+        example.setIsActive(false);
+        example.setStatus(0);
 
-        exampleService.update(example);
+        exampleRepository.save(example);
     }
 }
