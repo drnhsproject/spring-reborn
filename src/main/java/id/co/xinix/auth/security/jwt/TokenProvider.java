36881f9484dc94e3f072fd45c48cc9fa5372e3fe
drnhsproject <dian.rahmat74@gmail.com>
2025-06-30 11:05:06 +0700

    fix: delete unused import

--- src/main/java/id/co/xinix/auth/security/jwt/TokenProvider.java
+++ src/main/java/id/co/xinix/auth/security/jwt/TokenProvider.java
@@ -1,6 +1,5 @@
 package id.co.xinix.auth.security.jwt;
 
-import id.co.xinix.auth.exception.UnauthorizedException;
 import id.co.xinix.auth.management.SecurityMetersService;
 import id.co.xinix.auth.modules.authenticate.domain.UserDetail;
 import id.co.xinix.auth.modules.user.domain.User;
