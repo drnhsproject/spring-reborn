ebce19da8268cff5574e0ee918d12e0d619e973f
drnhsproject <dian.rahmat74@gmail.com>
2025-07-01 11:13:30 +0700

    fix: hide documentation user profile

--- src/main/java/id/co/xinix/auth/modules/userprofile/domain/UserProfileRepository.java
+++ src/main/java/id/co/xinix/auth/modules/userprofile/domain/UserProfileRepository.java
@@ -1,11 +1,13 @@
 package id.co.xinix.auth.modules.userprofile.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
+@RepositoryRestResource(exported = false)
 public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
     Optional<UserProfile> findByUserId(Long userId);
 }
--- src/main/java/id/co/xinix/auth/modules/userprofile/infrastructure/rest/UserProfileResource.java
+++ src/main/java/id/co/xinix/auth/modules/userprofile/infrastructure/rest/UserProfileResource.java
@@ -5,14 +5,17 @@ import id.co.xinix.auth.modules.userprofile.application.dto.UserProfileCreatedRe
 import id.co.xinix.auth.modules.userprofile.application.usecase.CreateUserProfile;
 import id.co.xinix.spring.services.SingleResponse;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
 import jakarta.validation.Valid;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 
+@Hidden
 @RestController
 @RequestMapping("/api/user-profiles")
 @RequiredArgsConstructor
