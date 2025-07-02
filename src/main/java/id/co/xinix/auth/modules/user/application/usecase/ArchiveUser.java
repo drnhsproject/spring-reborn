f57bf721038763ea2001735d62ae04ddacead735
Naufal Nur Hidayat <naufalnurhidayat@Naufals-MacBook-Pro.local>
2025-07-02 09:35:47 +0700

    fix(user): archive user role and user profile

--- src/main/java/id/co/xinix/auth/modules/user/application/usecase/ArchiveUser.java
+++ src/main/java/id/co/xinix/auth/modules/user/application/usecase/ArchiveUser.java
@@ -4,15 +4,23 @@ import id.co.xinix.auth.UseCase;
 import id.co.xinix.auth.exception.NotFoundException;
 import id.co.xinix.auth.modules.user.domain.User;
 import id.co.xinix.auth.modules.user.domain.UserRepository;
+import id.co.xinix.auth.modules.userprofile.domain.UserProfile;
+import id.co.xinix.auth.modules.userprofile.domain.UserProfileRepository;
+import id.co.xinix.auth.modules.userrole.domain.UserRole;
+import id.co.xinix.auth.modules.userrole.domain.UserRoleRepository;
+import lombok.AllArgsConstructor;
+
+import java.util.Set;
 
 @UseCase
+@AllArgsConstructor
 public class ArchiveUser {
 
     private final UserRepository userRepository;
 
-    public ArchiveUser(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    private final UserRoleRepository userRoleRepository;
+
+    private final UserProfileRepository userProfileRepository;
 
     public void handle(Long id) {
         User user = userRepository.findById(id)
@@ -22,5 +30,21 @@ public class ArchiveUser {
         user.setStatus(0);
 
         userRepository.save(user);
+
+        Set<UserRole> userRoles = userRoleRepository.findByUserId(user.getId());
+        for (UserRole userRole : userRoles) {
+            userRole.setIsActive(false);
+            userRole.setStatus(0);
+            userRoleRepository.save(userRole);
+        }
+
+        UserProfile userProfile = userProfileRepository.findByUserId(user.getId())
+            .orElse(null);
+
+        if (userProfile != null) {
+            userProfile.setIsActive(false);
+            userProfile.setStatus(0);
+            userProfileRepository.save(userProfile);
+        }
     }
 }
